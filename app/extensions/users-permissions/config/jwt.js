c8bb0703c747de333454104432f4c92dcdb69dad
hryer <harryermawan@gmail.com>
2020-08-03 15:18:30 +0700

    feat(setup): new setup

--- app/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'd61aa6b6-fa73-4491-97b1-dc6b6f88d271'
-};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f8e990fa-5bb1-4bf9-b5b0-83e5c35c67ea'
+};
